4ee82087c476772ad093808ed8926975519c3def
mfpfakhri <muhammad.fakhrian@gmail.com>
2018-03-08 19:37:43 +0700

    1th Commit : Interface CRUD DONE! 2nd Commit : Sign Up User Firebase DONE! 3rd Commit : Sign In User Firebase DONE! 4th Commit : Sign Out User Firebase DONE! 5th Commit : Reset Password Email Firebase DONE! 6th Commit : Sign Out User Firebase Edit Using Three Dots Menu DONE! 7th Commit : Change Password User Firebase DONE! Pattern Change Password Not Yet 8th Commit : Session Saved DONE! Pattern Change Password DONE! BUG on Change Password Not Yet 9th Commit : BUG on Change Password Using 1 EditText, Pattern Change Password DONE! 10th Commit : Nav. Drawer Using Activity, onBackPressed DONE! BUG on onBackPressed ChangePassword Not Yet 11th Commit : onBackPressed behaviour on ChangePassword Sequence (SignInActivity & LandingActivity) DONE! 12th Commit : Fix onBackPressed Method on SignUpActivity DONE! 13th Commit : AddProvince DONE! AddCity Not Yet 14th Commit : AddCity Layout DONE! There's Still an Error Storing City Data to Firebase 15th Commit : There's Still an Error Storing City Data to Firebase 16th Commit : There's Still an Error Storing City Data to Firebase. Using cityProvince on city Attribute 17th Commit : Using cityProvince on city Attribute 18th Commit : Sign Up User Validation on PROGRESS! 19th Commit : Gender Validation on Sign Up User on PROGRESS! 20th Commit : Gender Validation DONE! Alhamdulillah 21st Commit : Datepicker getDate and Datepicker Validation DONE! Alhamdulillah 22nd Commit : User Sign Up Interface DONE! Alhamdulillah 23th Commit : UserPhoto and IdCardPhoto Intent DONE! Alhamdulillah. Cropped Image and Get Image Not Yet 24th Commit : Cropped Image & Get Image DONE! Revision on Aspect Ratio, Alhamdulillah. Push to Firebase Not Yet 25th Commit : Profile Photos Validation DONE! Save to Firebase Not Yet 26th Commit : User Sign Up DONE! Bug on Firebase Storage Directory and App Crash when Sign In Automatically after Sign Up 27th Commit : App Crash Caused by AsyncTask DONE! Firebase Storage Directory & Images Position Stored Not Yet 28th Commit : After Sign Up Activity DONE! Firebase Storage Directory & Images Position Stored Not Yet 29th Commit : Firebase Storage Directory DONE! progressDialog & Images Position Not Yet 30th Commit : progressDialog Crash DONE! FirebaseStorage ImagePosition Not Yet 31th Commit : FirebaseStorage ImagePosition DONE! Happen only to Some Case

--- app/src/main/java/clouwiko/dev/prasiku/activity/activity/SignUpActivity.java
+++ app/src/main/java/clouwiko/dev/prasiku/activity/activity/SignUpActivity.java
@@ -40,7 +40,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnPausedListener;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
