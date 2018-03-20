85bde8220bb04bb3b5e6ffa66d0184e52fac6072
mfpfakhri <muhammad.fakhrian@gmail.com>
2018-03-20 14:52:30 +0700

    1st Commit : Interface CRUD DONE! 2nd Commit : Sign Up User Firebase DONE! 3rd Commit : Sign In User Firebase DONE! 4th Commit : Sign Out User Firebase DONE! 5th Commit : Reset Password Email Firebase DONE! 6th Commit : Sign Out User Firebase Edit Using Three Dots Menu DONE! 7th Commit : Change Password User Firebase DONE! Pattern Change Password Not Yet 8th Commit : Session Saved DONE! Pattern Change Password DONE! BUG on Change Password Not Yet 9th Commit : BUG on Change Password Using 1 EditText, Pattern Change Password DONE! 10th Commit : Nav. Drawer Using Activity, onBackPressed DONE! BUG on onBackPressed ChangePassword Not Yet 11st Commit : onBackPressed behaviour on ChangePassword Sequence (SignInActivity & LandingActivity) DONE! 12nd Commit : Fix onBackPressed Method on SignUpActivity DONE! 13rd Commit : AddProvince DONE! AddCity Not Yet 14th Commit : AddCity Layout DONE! There's Still an Error Storing City Data to Firebase 15th Commit : There's Still an Error Storing City Data to Firebase 16th Commit : There's Still an Error Storing City Data to Firebase. Using cityProvince on city Attribute 17th Commit : Using cityProvince on city Attribute 18th Commit : Sign Up User Validation on PROGRESS! 19th Commit : Gender Validation on Sign Up User on PROGRESS! 20th Commit : Gender Validation DONE! Alhamdulillah 21st Commit : Datepicker getDate and Datepicker Validation DONE! Alhamdulillah 22nd Commit : User Sign Up Interface DONE! Alhamdulillah 23rd Commit : UserPhoto and IdCardPhoto Intent DONE! Alhamdulillah. Cropped Image and Get Image Not Yet 24th Commit : Cropped Image & Get Image DONE! Revision on Aspect Ratio, Alhamdulillah. Push to Firebase Not Yet 25th Commit : Profile Photos Validation DONE! Save to Firebase Not Yet 26th Commit : User Sign Up DONE! Bug on Firebase Storage Directory and App Crash when Sign In Automatically after Sign Up 27th Commit : App Crash Caused by AsyncTask DONE! Firebase Storage Directory & Images Position Stored Not Yet 28th Commit : After Sign Up Activity DONE! Firebase Storage Directory & Images Position Stored Not Yet 29th Commit : Firebase Storage Directory DONE! progressDialog & Images Position Not Yet 30th Commit : progressDialog Crash DONE! FirebaseStorage ImagePosition Not Yet 31st Commit : FirebaseStorage ImagePosition DONE! Happen only to Some Case 32nd Commit : Firebase Auth Email Check DONE! 33rd Commit : Attached provinceKey to cities Table DONE!!! Alhamdulillah. Retrieve Profile Photo on Home Activity & AutoCompleteTextView Firebase Query on Sign Up Revision Not Yet 34th Commit : AutoCompleteTextView Firebase Query on Sign Up DONE! Alhamdulillah. Retrieve Profile Photo on Home Activity Not Yet 35th Commit : Main Menu Interface DONE! 36th Commit : Main Menu Interface Revision DONE! 37th Commit : CardView Intent to Activity on MainMenuActivity DONE! 38th Commit : onBackPressed on MainMenuActivity Improved DONE! 39th Commit : UploadCatData Interface DONE! 40th Commit : UploadCatData Form Validation DONE! 41st Commit : There is Bug on UserRegistration Query 42nd Commit : User Table uID Firebase DONE! Cats Table uID Not Yet. Try SharedPreferences! 43rd Commit : Push UID DONE! Updating User Data and Check if Already Complete User Information Not Yet 44th Commit : Push UID DONE! Using One Page Sign Up and Directly to MainMenuActivity After Sign Up. UploadCatData on Progress 45th Commit : UploadCatData DONE! Retrieve User Data on NavDraw and UserHomeActivity not Yet 46th Commit : Retrieve User Data on NavDraw got Issue. 45th Commit : UI UserHome DONE! Cat Fragment, User Profile and Retrieve for both Fragment Not Yet

--- app/src/main/java/clouwiko/dev/prasiku/activity/activity/AppSubmittedActivity.java
+++ app/src/main/java/clouwiko/dev/prasiku/activity/activity/AppSubmittedActivity.java
@@ -2,6 +2,7 @@ package clouwiko.dev.prasiku.activity.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import clouwiko.dev.prasiku.R;
 
@@ -11,5 +12,10 @@ public class AppSubmittedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_submitted);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Application Received");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
